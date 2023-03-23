e961c0209ce79601bac9355d0e5bb5f050a9cee1
Tobin Waters <tkwaterss@gmail.com>
2023-03-23 16:30:24 -0600

    form hides and fixed some state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import Reset from "./components/Reset";
 import Square from "./components/Square";
-import PlayerForm from './components/PlayerForm';
+import NewPlayers from "./components/NewPlayers";
 import "./App.css";
 
 function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
-  const [playerOne, setPlayerOne] = useState('');
-  const [playerTwo, setPlayerTwo] = useState('');
+  const [players, setPlayers] = useState(['','']);
 
   const handleReset = () => {
     setSquares(["", "", "", "", "", "", "", "", ""]);
     setPlayer(true);
   };
 
-  const addPlayers = (playerOne, playerTwo) => {
-    setPlayerOne(playerOne);
-    setPlayerTwo(playerTwo);
+  const addPlayers = (players) => {
+    setPlayers(players)
   }
 
   const checkWinner = (array) => {
@@ -34,9 +32,9 @@ function App() {
       const [a ,b, c] = lines[i];
       if( array[a] && array[a] === array[b] && array[a] === array[c]) {
         if(array[a] === "X") {
-          return `${playerOne} Won!`
+          return `${players[0] ? players[0] : 'X'} Won!`
         } else {
-          return `${playerTwo} Won!`
+          return `${players[1] ? players[1] : 'O'} Won!`
         }
       } 
     }
@@ -45,10 +43,10 @@ function App() {
 
   return (
     <div className="App">
-      <PlayerForm addPlayers={addPlayers}/>
+      <NewPlayers addPlayers={addPlayers}/>
       <div className="name-container">
-        <h2>{`Player 1 (X): ${playerOne}`}</h2>
-        <h2>{`Player 2 (O): ${playerTwo}`}</h2>
+        <h2>{players[0] ? `Player 1 (X): ${players[0]}` : ''}</h2>
+        <h2>{players[1] ? `Player 2 (O): ${players[1]}` : ''}</h2>
       </div>
       <Reset handleReset={handleReset}/>
       <div className="container">
